906f31e80144dba08ea6e6f76246301d32f43b12
DanhNguyenz <danh.nt.67khmt@ntu.edu.vn>
2025-11-23 19:22:27 +0700

    Create kiem_tra_so_duong.py

---
+++ kiem_tra_so_duong.py
@@ -0,0 +1,6 @@
+number = float(input("Hãy nhập số ngẫu nhiên:"))
+               
+if number > 0:
+               print ("Đây là số dương")
+else:
+               print ("Đây không phải là số dương")
