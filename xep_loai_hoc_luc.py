07f0c9914f1ed4e4a263d2dc75e81db3ae733114
DanhNguyenz <danh.nt.67khmt@ntu.edu.vn>
2025-11-23 22:21:40 +0700

    Create xep_loai_hoc_luc.py

---
+++ xep_loai_hoc_luc.py
@@ -0,0 +1,10 @@
+Điểm = float(input("Nhập điểm trung bình: "))
+
+if Điểm >= 8.0:
+    print("Học lực: Giỏi")
+elif Điểm >= 6.5:
+    print("Học lực: Khá")
+elif Điểm >= 5.0:
+    print("Học lực: Trung bình")
+else:
+    print("Học lực: Yếu")
