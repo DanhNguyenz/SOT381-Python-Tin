edfaf8e86f686764f846a00a0d2ac73a88ca810c
DanhNguyenz <danh.nt.67khmt@ntu.edu.vn>
2025-11-23 22:29:32 +0700

    Create kiem_ngay_trong_tuan.py

---
+++ kiem_ngay_trong_tuan.py
@@ -0,0 +1,18 @@
+Ngày = float(input("Hãy nhập số ngày:"))
+
+if Ngày == 2:
+    print("Thứ Hai")
+elif Ngày == 3:
+    print("Thứ Ba")
+elif Ngày == 4:
+    print("Thứ Tư")
+elif Ngày == 5:
+    print("Thứ Năm")
+elif Ngày == 6:
+    print("Thứ Sáu")
+elif Ngày == 7:
+    print("Thứ Bảy")
+elif Ngày == 8:
+    print("Chủ Nhật")
+else:
+    print("Lỗi!")
