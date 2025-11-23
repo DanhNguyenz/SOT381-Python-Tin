057485794ef79dc6a689ac3e950f2270579a689a
DanhNguyenz <danh.nt.67khmt@ntu.edu.vn>
2025-11-23 18:52:04 +0700

    Update MerryXmas.py

--- MerryXmas.py
+++ MerryXmas.py
@@ -1,7 +1,2 @@
-
-number = int(input("Nhập vào một số ngẫu nhiên: "))
-
-if number % 2 == 0:
-    print(x"Số {number} là số chẵn.")
-else:
-    print(x"Số {number} là số lẻ.")
+print ("Merry Christmas 2025")
+print ("Nha Trang University")
