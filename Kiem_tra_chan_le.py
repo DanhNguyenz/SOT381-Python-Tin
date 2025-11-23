6e6a941d8b913962b76d6ffe80c7047a4a871e58
DanhNguyenz <danh.nt.67khmt@ntu.edu.vn>
2025-11-23 18:49:31 +0700

    kiem_tra_chan_le

---
+++ Kiem_tra_chan_le.py
@@ -0,0 +1,6 @@
+number = int(input("Nhập vào một số nguyên: "))
+
+if number % 2 == 0:
+    print(f"Số {number} là số chẵn.")
+else:
+    print(f"Số {number} là số lẻ.")
--- MerryXmas.py
+++ MerryXmas.py
@@ -1,2 +1,7 @@
-print ("Merry Christmas 2025")
-print ("Nha Trang University")
+
+number = int(input("Nhập vào một số ngẫu nhiên: "))
+
+if number % 2 == 0:
+    print(x"Số {number} là số chẵn.")
+else:
+    print(x"Số {number} là số lẻ.")
