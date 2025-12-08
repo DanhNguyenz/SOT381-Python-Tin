04466b774b97337d21ba2e9500774785b8a9b4f4
DanhNguyenz <danh.nt.67khmt@ntu.edu.vn>
2025-12-08 11:19:48 +0700

    bt

--- Tinh_tong_bphuong_1_toi_n.py
+++ Tinh_tong_bphuong_1_toi_n.py
@@ -1,3 +1,3 @@
 n = int(input("Nhập n: "))
-Kết quả = sum([i**2 for i in range(1, n + 1, 2)])
-print(f"Kết quả là: {Kết quả}")
+ket_qua = sum([i**2 for i in range(1, n + 1, 2)])
+print(f"Kết quả là: {ket_qua}")
